1f0336af1974c30ab1bc8287ec3ccffb111ae2cc
Carlosfire668 <CarlosEduardo07171@outlook.com>
2025-01-15 15:52:54 +0900

    frontend

--- frontend/src/auth/register.js
+++ frontend/src/auth/register.js
@@ -26,7 +26,7 @@ export const Register = () => {
       // Callbacks can be added here
       if (data) {
         await auth.login(email, password);
-        alert('Register User Successful!');
+        alert('Register Successful!');
         history.push('/');
       }
     }
